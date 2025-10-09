51c9f3cdd205a8c6b1261ca390bf75db49cc5248
Ivan-Vakhula07 <vakhulaivan2007@gmail.com>
2025-10-09 08:44:23 +0300

    Lab 1 commit

---
+++ app/js/script.js
@@ -0,0 +1,9 @@
+// Код для тестування мінімізації (gulp-uglify)
+function checkAutomation() {
+    const status = "Gulp is running successfully!";
+
+    // Перевірка роботи JS у консолі
+    console.log("JS TASK STATUS:", status);
+}
+
+checkAutomation();
