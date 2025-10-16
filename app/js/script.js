8680c2a4f7279d2b68d2725339fd99c9b0260e15
Petro Topylko <petrotopylko@gmail.com>
2025-10-16 09:44:55 +0300

    Delete app directory

--- app/js/script.js
+++
@@ -1,9 +0,0 @@
-// Код для тестування мінімізації (gulp-uglify)
-function checkAutomation() {
-    const status = "Gulp is running successfully!";
-
-    // Перевірка роботи JS у консолі
-    console.log("JS TASK STATUS:", status);
-}
-
-checkAutomation();
