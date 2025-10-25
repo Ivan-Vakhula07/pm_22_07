85a1755bd5ebfc84c278526fd61e252a80d6c186
Ivan-Vakhula07 <vakhulaivan2007@gmail.com>
2025-10-25 10:37:07 +0300

    Ваше повідомлення про зміни

--- src/app/js/script.js
+++ src/app/js/script.js
@@ -1,9 +1,5 @@
-// Код для тестування мінімізації (gulp-uglify)
 function checkAutomation() {
     const status = "Gulp is running successfully!";
-
-    // Перевірка роботи JS у консолі
     console.log("JS TASK STATUS:", status);
 }
-
 checkAutomation();
